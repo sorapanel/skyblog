a0dd9a68a2be53dfc47b52de7141efb47d4bd2b1
Sora <91284120+sorapanel@users.noreply.github.com>
2023-05-29 22:24:11 +0900

    second commit
    BDjango/settings.pyにていくつかエラー（Herokuに移行する際に加えたstaticファイルに関する設定まわり）があり、サーバーが立ち上げられなかったため修正しています。

--- BDjango/settings.py
+++ BDjango/settings.py
@@ -120,10 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [STATIC_DIR,]
-
-django_on_heroku.settings(locals)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
