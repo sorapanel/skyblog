b798eee0d345e8ca04ce32109b5efb1afe7b7787
Sora <sky.roofcommon.0314@gmail.com>
2023-06-06 11:21:00 +0900

    add function that login system to use TwitterAPI

--- BDjango/urls.py
+++ BDjango/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Registration.urls')),
+    path('', include('social_django.urls', namespace='social')),
     path('blog/', include('Blog.urls')),
 ]
