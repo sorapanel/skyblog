746c9d091c49e4b93e16748823588cf8932e27a8
Sora <sky.roofcommon.0314@gmail.com>
2023-06-06 15:46:33 +0900

    arrage templates on Blog and Registration

--- Blog/views.py
+++ Blog/views.py
@@ -10,23 +10,26 @@ from .models import Post
 class Index(ListView):
     # 一覧するモデルを指定 -> `object_list`で取得可能
     model = Post
+    template_name = 'Blog/post_list.html'
 
 # DetailViewは詳細を簡単に作るためのView
 class Detail(DetailView):
     # 詳細表示するモデルを指定 -> `object`で取得可能
     model = Post
+    template_name = 'blog/post_detail.html'
 
 class Create(CreateView):
     model = Post
-    
+    template_name = 'Blog/post_form.html'
     # 編集対象にするフィールド
     fields = ["title", "body", "category", "tags"]
 
 class Update(UpdateView):
     model = Post
+    template_name = 'Blog/post_detail.html'
     fields = ["title", "body", "category", "tags"]
 
 class Delete(DeleteView):
     model = Post
-    
+    template_name = 'Blog/post_confirm_delete.html'
     success_url = "/blog/list/"
